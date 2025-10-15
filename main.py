7b05066bdc114e18890218afdc7da92b2d884437
Limbachiya Kartik <117003231+Kartik-Limbachiya@users.noreply.github.com>
2025-10-15 19:02:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,3 @@
-# from fastapi import FastAPI, File, UploadFile, Form
-# from fastapi.responses import JSONResponse
-# from parser import CreditCardStatementParser
-# import os
-# import tempfile
-# import uvicorn
-
-# app = FastAPI(
-#     title="Credit Card Statement Parser API",
-#     description="Extracts key info from credit card statements (Axis, BOB, Kotak, SBI)",
-#     version="1.0.0"
-# )
-
-# parser = CreditCardStatementParser()
-
-# @app.post("/parse-statement/")
-# async def parse_statement(bank: str = Form(...), file: UploadFile = File(...)):
-#     """
-#     Upload a PDF and specify the bank (axis, bob, kotak, sbi).
-#     Example: 
-#         curl -X POST -F "bank=axis" -F "file=@Axis1-unlocked.pdf" https://yourapi.onrender.com/parse-statement/
-#     """
-#     try:
-#         # Save file temporarily
-#         with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
-#             tmp.write(await file.read())
-#             tmp_path = tmp.name
-
-#         # Parse PDF
-#         result = parser.parse_statement(tmp_path, bank)
-
-#         # Clean up temp file
-#         os.remove(tmp_path)
-
-#         return JSONResponse(content=result.to_dict())
-
-#     except Exception as e:
-#         return JSONResponse(status_code=500, content={"error": str(e)})
-
-
-# @app.get("/")
-# async def root():
-#     return {"message": "Welcome to Credit Card Parser API 🚀", "usage": "/parse-statement/"}
-
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
-
-
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware # Import CORSMiddleware
@@ -65,8 +16,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",  # Allow your local React app
     "http://localhost",
-    # Add the URL of your deployed front-end application here
-    # Example: "https://your-frontend-app-name.vercel.app",
+    "credit-card-statement-parser-hjauuf3qc.vercel.app",
 ]
 
 app.add_middleware(
